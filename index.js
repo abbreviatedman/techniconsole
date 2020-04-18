d2727e18d6a1f3c6411854814dcb934acc2b266d
Colin Jaffe <balloonasaurus@gmail.com>
2020-04-17 20:40:26 -0400

    Adds .banner and .byItself and renamed .label to .withLabel.

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
-const colors = require('colors');
+const colors = require( 'colors' );
+const termSize = require( 'term-size' )
+const wordWrap = require( 'wordwrap' )
+const boxen = require( 'boxen' )
 
 
 const techniconsole = {
-  label: (values) => {
-    Object.keys(values).forEach((key) => {
-      console.log(colors.magenta(key))
-      console.log(colors.magenta.bold('-> '), colors.cyan(values[key]))
-      console.log('')
+  withLabel( values ) {
+    Object.keys( values ).forEach( ( key ) => {
+      console.log( colors.magenta( key ) )
+      console.log( colors.magenta.bold( '-> ' ), colors.cyan( values[key] ) )
+      console.log( '' )
     })
+  },
+
+  byItself ( str ) {
+    console.clear()
+    console.log( str )
+  },
+
+  banner: ( str ) => {
+    const rowWidth = termSize().columns
+    const lines = wordWrap( rowWidth - 10 )(str)
+    const strWidth = Math.min( str.length, rowWidth - 10 )
+    const horizontalPadding = Math.round( ( rowWidth - strWidth ) / 2 ) - 5
+
+    const options = {
+      borderColor: 'magenta',
+      float: 'center',
+      align: 'center',
+      borderStyle: 'double',
+      padding: {
+        top: 1,
+        bottom: 1,
+        left: horizontalPadding,
+        right: horizontalPadding,
+      },
+    }
+
+
+    console.log(
+      colors.cyan(
+        boxen( lines.toUpperCase(), options )
+      )
+    )
+
   },
 }
 
