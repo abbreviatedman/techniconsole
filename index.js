32f367c8e520899cc0ef0a57decfb57e32a71948
Colin Jaffe <balloonasaurus@gmail.com>
2020-04-19 16:39:45 -0400

    Initial commit for develop branch.

--- index.js
+++ index.js
@@ -1,13 +1,71 @@
 const colors = require( 'colors' );
+const termSize = require( 'term-size' )
+const wordWrap = require( 'wordwrap' )
+const boxen = require( 'boxen' )
+
 
 
 const techniconsole = {
-  label( variables ) {
+  withLabel( variables ) {
     Object.keys( variables ).forEach( ( variableName ) => {
       const value = variables[variableName]
       console.log( colors.magenta( variableName ) )
       console.log( colors.magenta.bold( '-> ' ), colors.cyan( value ) )
       console.log( '' )
+    })
+  },
+
+  atTop ( str ) {
+    console.clear()
+    console.log( str )
+  },
+
+  asBanner ( str ) {
+    const rowWidth = termSize().columns
+    const rows = wordWrap( rowWidth - 10 )( str )
+    const strWidth = Math.min( str.length, rowWidth - 10 )
+    const horizontalPadding = Math.round( ( rowWidth - strWidth ) / 2 ) - 5
+
+    const options = {
+      borderColor: 'magenta',
+      float: 'center',
+      align: 'center',
+      borderStyle: 'double',
+      padding: {
+        top: 1,
+        bottom: 1,
+        left: horizontalPadding,
+        right: horizontalPadding,
+      },
+    }
+
+    console.log(
+      colors.cyan(
+        boxen( rows.toUpperCase(), options )
+      )
+    )
+  },
+
+  log ( ...vals ) {
+    vals.forEach((val) => {
+      console.log(`                                  (@@@@@@@@
+                                  (@@    @@@/                                   
+                                  (@@      @@@                                  
+          @@@@@@@@@@@@@@@@@@@@@@@@@@@        @@@@@@@@@@@@@@@@@@@@@@@@@@         
+      *@@@@               @@@@                                        @@@@      
+    @@@@    @@@@@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.       @@@@   
+  &@@    @@@@          @@@@,   @@@                                         &@@  
+ @@@   @@@     @@@@@@@    @@@   %@@                                          @@(
+ @@   @@@   @@@      @@@/   @@   @@@@@@@@@@@@@@@@@@@@   *@@@@@@@@@@@@@@@@     @@
+@@@   @@   @@@         @@   @@@   @@                                          @@
+@@@   @@   @@@         @@   @@@  .@@                 @@@@@@@@@@@@             @@
+ @@   @@@   %@@@    @@@@   %@@   @@@  @@@@@@@@@@@@@@@@         .@@@@@@@@@    #@@
+ *@@    @@@    @@@@@@     @@@   @@@                    @@@@@@@@             ,@@ 
+   @@@    @@@@                 @@@                    @@#    @@@           @@@  
+    (@@@     @@@@@@@@@@     .@@@@@@@@@@@@@@@@@@@@                        @@@    
+       @@@@@             @@@@/                                       @@@@@      
+           @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@          `)
+      this.withLabel({[`Well... you did ask for a log!`]: val})
     })
   },
 }
