11be7bd66bed09d05a4d496d9ba9c384009da5ab
Colin Jaffe <balloonasaurus@gmail.com>
2020-04-19 12:14:42 -0400

    Moving changes to spec to new branch.

--- index.js
+++ index.js
@@ -1,51 +1,14 @@
 const colors = require( 'colors' );
-const termSize = require( 'term-size' )
-const wordWrap = require( 'wordwrap' )
-const boxen = require( 'boxen' )
 
 
 const techniconsole = {
-  withLabel( variables ) {
+  label( variables ) {
     Object.keys( variables ).forEach( ( variableName ) => {
       const value = variables[variableName]
       console.log( colors.magenta( variableName ) )
       console.log( colors.magenta.bold( '-> ' ), colors.cyan( value ) )
       console.log( '' )
     })
-  },
-
-  byItself ( str ) {
-    console.clear()
-    console.log( str )
-  },
-
-  banner: ( str ) => {
-    const rows = wordWrap( rowWidth - 10 )(str)
-
-    const rowWidth = termSize().columns
-    const strWidth = Math.min( str.length, rowWidth - 10 )
-    const horizontalPadding = Math.round( ( rowWidth - strWidth ) / 2 ) - 5
-
-    const options = {
-      borderColor: 'magenta',
-      float: 'center',
-      align: 'center',
-      borderStyle: 'double',
-      padding: {
-        top: 1,
-        bottom: 1,
-        left: horizontalPadding,
-        right: horizontalPadding,
-      },
-    }
-
-
-    console.log(
-      colors.cyan(
-        boxen( rows.toUpperCase(), options )
-      )
-    )
-
   },
 }
 
