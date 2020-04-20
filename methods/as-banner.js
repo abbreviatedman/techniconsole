e96b99cd9216474ea545ca47b25dfadc1b98d4fd
Colin Jaffe <balloonasaurus@gmail.com>
2020-04-19 21:22:25 -0400

    Adds ability to manually split rows for asBanner.

--- methods/as-banner.js
+++ methods/as-banner.js
@@ -4,12 +4,18 @@ const colors = require ( 'colors' )
 const boxen = require ( 'boxen' )
 
 
-const asBanner = function ( str ) {
-  const rowWidth = termSize().columns
-  const rows = wordWrap ( rowWidth - 10 ) ( str )
-  const strWidth = Math.min ( str.length, rowWidth - 10 )
-  const horizontalPadding = Math.round ( ( rowWidth - strWidth ) / 2 ) - 5
+const asBanner = function ( ...strs ) {
+  const termWidth = termSize (  ).columns
+  const rows = Array.from ( strs ).map ( ( str ) => wordWrap ( termWidth - 10 ) ( str.toUpperCase (  ) ) )
+  let greatestLength = 0;
+  for ( const row of rows ) {
+    for ( const subStr of row.split ( '\n' ) ) {
+      greatestLength = Math.max ( greatestLength, subStr.length )
+    }
+  }
 
+  const strWidth = Math.min ( greatestLength, termWidth - 10 )
+  const horizontalPadding = Math.round ( ( termWidth - strWidth ) / 2 ) - 5
   const options = {
     borderColor: 'magenta',
     float: 'center',
@@ -17,15 +23,15 @@ const asBanner = function ( str ) {
     borderStyle: 'double',
     padding: {
       top: 1,
+      right: horizontalPadding,
       bottom: 1,
       left: horizontalPadding,
-      right: horizontalPadding,
     },
   }
 
-  console.log(
+  console.log (
     colors.cyan (
-      boxen ( rows.toUpperCase (), options )
+      boxen ( rows.join ( '\n' ), options )
     )
   )
 }
