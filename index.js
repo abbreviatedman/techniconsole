53432bd8d4b50b864dd0ec1d157c963da8e9ca18
Colin Jaffe <balloonasaurus@gmail.com>
2020-04-28 17:41:01 -0400

    Re-adds label as an alias for the new withLabel.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const withLabel = require ( './methods/with-label.js' )
+const label = require ( './methods/with-label.js' )
 const atTop = require ( './methods/at-top.js' )
 const asBanner = require ( './methods/as-banner.js' )
 const log = require ( './methods/log.js' )
 
 
 const techniconsole = {
+  label,
   withLabel,
   atTop,
   asBanner,
