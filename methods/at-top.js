bb6c61ee792cbd47887148b0a22318e9892b671b
Colin Jaffe <balloonasaurus@gmail.com>
2020-05-05 14:59:04 -0400

    Adds explanatory message for clearing logs.

--- methods/at-top.js
+++ methods/at-top.js
@@ -3,6 +3,8 @@ const colors = require ( 'colors' )
 
 const atTop = function ( ...strs ) {
   console.clear (  )
+  console.log( colors.cyan( `We've cleared all logs above here to show you the following value(s):` ) )
+  console.log( '' )
   strs.forEach ( ( str ) => console.log ( colors.bgMagenta.cyan.bold ( str ) ) )
 }
 
