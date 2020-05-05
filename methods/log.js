b2e849edce9fa8d5ddc952d55c77e72bfb1af021
Colin Jaffe <balloonasaurus@gmail.com>
2020-05-05 14:50:47 -0400

    Makes "erorr message" more helpful and informative.

--- methods/log.js
+++ methods/log.js
@@ -20,8 +20,14 @@ const log = function ( ...vals ) {
     (@@@     @@@@@@@@@@     .@@@@@@@@@@@@@@@@@@@@                        @@@    
        @@@@@             @@@@/                                       @@@@@      
            @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@          `)
-  console.log ( colors.cyan ( 'You asked for a log...' ) )
-  vals.forEach((val) => console.log ( colors.magenta( val ) ) )
+  console.log ( colors.cyan ( `Well... you DID ask for a log! You probably wanted either CONSOLE.log or one of our OTHER methods, like:` ))
+  console.log( colors.cyan( `*` ), colors.magenta(`.withLabel` ) )
+  console.log( colors.cyan( `*` ), colors.magenta(`.asBanner` ) )
+  console.log( colors.cyan( `*` ), colors.magenta(`.atTop` ) )
+  console.log( '' )
+  console.log ( colors.cyan ( `Anyway... here's the value you wanted "logged"!` ) )
+  console.log( '' )
+  vals.forEach( ( val ) => console.log ( colors.magenta( val ) ) )
 }
 
 
