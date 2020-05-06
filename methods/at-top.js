4a95010ea679f3a7cba04107a0f298b984163fa6
Colin Jaffe <balloonasaurus@gmail.com>
2020-05-05 20:07:16 -0400

    Changes colors for readability.

--- methods/at-top.js
+++ methods/at-top.js
@@ -3,9 +3,9 @@ const colors = require ( 'colors' )
 
 const atTop = function ( ...strs ) {
   console.clear (  )
-  console.log( colors.cyan( `You've used .atTop to clear all previous logs, highlighting the following value(s):` ) )
+  console.log( colors.red( `You've used .atTop to clear all previous logs, highlighting the following value(s):` ) )
   console.log( '' )
-  strs.forEach ( ( str ) => console.log ( colors.bgMagenta.cyan.bold ( str ) ) )
+  strs.forEach ( ( str ) => console.log ( colors.bgBlue.red ( str ) ) )
 }
 
 
